0697f44fbc98042b83c8f1140472d1bfbcc65091
reniebeanbean <mysteryheart0205@gmail.com>
2020-06-29 12:30:48 -0700

    git config --global core.editor "'C:/Program Files/Notepad++/notepad++.exe' -multiInst -notabbar -nosession -noPlugin"

---
+++ test.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+
+public class test {
+
+    public static void main(String[] args) {
+
+        ArrayList <Integer> digitList = new ArrayList<>();
+        int num = 15704;
+        int place = 0;
+        while (num > 0){
+            place++;
+            digitList.add(0,num%10);
+            num/=10;
+        }
+        System.out.println(place);
+        System.out.println(num);
+        
+        for (int s = 0; s < place; s++){
+            System.out.print(digitList.get(s) + ", ");
+        }
+    }
+}
