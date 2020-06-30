9f9cabecc6f6166a427e00bec8d281b797981378
107404JE <55158273+107404JE@users.noreply.github.com>
2020-06-30 18:59:13 -0400

    Update World.java
    Extended the World class by implementing a max heap to create somewhat of a hierarchial system based on a species' key

--- World.java
+++ World.java
@@ -1,3 +1,72 @@
-public class World
+import java.util.Comparator;
+
+public class World implements Comparator<Species>
 {
-}
+  private Species[] hierarchy;
+  private int pos;
+  private int capacity;
+  Species root;
+  
+  public World()
+  {
+    capacity = 100;
+    hierarchy = new Species[capacity];
+    pos = 1;
+    root = null;
+  }
+  @Override
+  public int compare(Species obj1, Species obj2)
+  {
+    if(obj1.getKey() < obj2.getKey())
+    {
+      return -1;
+    }
+    else if(obj1.getKey() > obj2.getKey())
+    {
+      return 1;
+    }
+    return 0;
+  }
+  
+  public Species left(Species s)
+  {
+    for(int i = 0; i<hierarchy.length; i++)
+    {
+      if(hierarchy[i] == s)
+      {
+        return hierarchy[2*i + 1];
+      }
+    }
+  }
+  public Species right(Species s)
+  {
+    for(int i = 0; i<hierarchy.length; i++)
+    {
+      if(hierarchy[i] == s)
+      {
+        return hierarchy[2*i + 2];
+      }
+    }
+  }
+  public void insert(Species s)
+  {
+    if(pos == capacity)
+    {
+      return;
+    }
+    else
+    {
+      hierarchy[pos++] = s;
+      int child = pos - 1;
+      int parent = child/2;
+      while(compare(hierarchy[parent], hierarchy[child]) == -1)
+      {
+        Species temp = hierarchy[parent];
+        hierarchy[parent] = hierarchy[child];
+        hierarchy[child] = temp;
+        child = parent;
+        parent = child/2;
+      }
+    }
+  }
+  
