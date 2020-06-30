203b67b369814295ccc3a2a41486e1884d5f2cfa
107404JE <55158273+107404JE@users.noreply.github.com>
2020-06-30 16:49:15 -0400

    Update Organism.java

--- Organism.java
+++ Organism.java
@@ -1,9 +1,9 @@
-public class Organism extends species {
+public class Organism{
 	// Instance Variables that can be used
 	
-	public Organism(String name, boolean isPlant, boolean isAquatic, boolean isCarnivorous)
+	public Organism()
 	{
-		super(name, isPlant, isAquatic, isCarnivorous);
+		
 	}
 	
 	/*Below the constructor will be some action methods. They could be like eating organisms from another species, reproduce, genetic mutation, etc*/
