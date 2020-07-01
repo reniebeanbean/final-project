961585c8a65e3821ebbda7bdbd47ffc649342c57
107404JE <55158273+107404JE@users.noreply.github.com>
2020-07-01 00:34:34 -0400

    Update Organism.java
    Added a reproduce method to serve as an example for the Organism class. I added a parameter (Type) to the constructor since most methods will rely on the type of species the organism belongs to

--- Organism.java
+++ Organism.java
@@ -1,10 +1,24 @@
 public class Organism{
 	// Instance Variables that can be used
+	private Species type;
 	
-	public Organism()
+	public Organism(Species type)
 	{
-		
+		this.type = type;
 	}
 	
 	/*Below the constructor will be some action methods. They could be like eating organisms from another species, reproduce, genetic mutation, etc*/
+	public void reproduce(Organism o)
+	{
+	    if(this.getType() == o.getType())
+	    {
+	        Organism newThing = new Organism(type);
+	        type.add(newThing);
+	        System.out.println("A new " + type.getName() + " was added");
+	    }
+	}
+	public Species getType()
+	{
+	    return type;
+	}
 }
