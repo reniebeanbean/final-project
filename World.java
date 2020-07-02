1f76c409fb5c70af90c0b24d46fbbcc87f9927c4
107404JE <55158273+107404JE@users.noreply.github.com>
2020-07-02 15:55:08 -0400

    Update World.java

--- World.java
+++ World.java
@@ -7,11 +7,22 @@ public class World implements Comparator<Species>
   private HashMap<Integer, ArrayList<Species>> hierarchy;
   private int capacity;
   private ArrayList<Species> allSpecies;
+  private int plants;
+  private int fish;
+  private int landAnimals;
   public World()
   {
-    capacity = 101;
+    capacity = 10;
     hierarchy = new HashMap<Integer, ArrayList<Species>>();
     allSpecies = new ArrayList<Species>();
+    ArrayList<Species> blank = new ArrayList<Species>();
+    for(int i = 1; i<=5; i++)
+    {
+        hierarchy.put(i, blank);
+    }
+    plants = 0;
+    fish = 0;
+    landAnimals = 0;
   }
   @Override
   public int compare(Species obj1, Species obj2)
@@ -46,9 +57,10 @@ public class World implements Comparator<Species>
         {
           if(allSpecies.get(j).getKey() == i)
           {
-            hierarchy.put(i, arr);
+            arr.add(allSpecies.get(j));
           }
         }
+        hierarchy.put(i,arr);
     }
     }
   }
@@ -60,7 +72,7 @@ public class World implements Comparator<Species>
       {
         if(hierarchy.get(i).get(j).getPopulation() == 0)
         {
-          System.out.println(hierarchy.get(i).remove(j) + "s went extinct");
+          System.out.println(hierarchy.get(i).remove(j).getName() + "s went extinct");
         }
       }
     }
@@ -69,8 +81,99 @@ public class World implements Comparator<Species>
   {
       return capacity;
   }
+  public void setCapacity(int k)
+  {
+      capacity = k;
+  }
   public HashMap<Integer,ArrayList<Species>> getHierarchy()
   {
       return hierarchy;
   }
+  public void addSpecies()
+  {
+      int choice = (int)(Math.random() * 2);
+        boolean isPlant = false;
+        boolean isAquatic = false;
+        boolean isCarnivorous = false;
+        if(choice > 0)
+        {
+            isPlant = true;
+            
+        }
+        choice = (int)(Math.random() * 2);
+        if(choice > 0)
+        {
+            isAquatic = true;
+        }
+        choice = (int)(Math.random() * 2);
+        if(choice > 0 && !isPlant)
+        {
+            isCarnivorous = true;
+        }
+        if(isPlant)
+        {
+            Species newSpecies = new Species("Plant " + plants, isPlant, isAquatic, isCarnivorous);
+            plants++;
+            this.insert(newSpecies);
+            System.out.println(newSpecies.getName() + " have been added");
+        }
+        else if(isAquatic)
+        {
+            Species newSpecies = new Species("Fish " + fish, isPlant, isAquatic, isCarnivorous);
+            fish++;
+            this.insert(newSpecies);
+            System.out.println(newSpecies.getName() + " have been added");
+        }
+        else
+        {
+            Species newSpecies = new Species("Animal " + landAnimals, isPlant, isAquatic, isCarnivorous);
+            landAnimals++;
+            this.insert(newSpecies);
+            System.out.println(newSpecies.getName() + " have been added");
+        }
+        
+  }
+  public void randomEvent(Organism o)
+    {
+        int event = (int)Math.random()*2;
+        int key = o.getType().getKey();
+        if(event == 0)
+        {
+          o.reproduce(this.getHierarchy().get(key).get(this.getHierarchy().get(key).indexOf(o.getType())).getRandomOrganism(o));
+        }
+        if(event == 1)
+        {
+            addSpecies();
+        }
+    }
+    public void event()
+    {
+        if(allSpecies.size() == 0)
+        {
+            addSpecies();
+        }
+        else
+        {
+            int event = (int)(Math.random()*2);
+            if(event == 0)
+            {
+                addSpecies();
+            }
+            if(event == 1)
+            {
+                for(int i = 1; i<=this.getHierarchy().size(); i++)
+                {
+                        for(int j = 0; j< this.getHierarchy().get(i).size(); j++)
+                        {
+                            Species s = this.getHierarchy().get(i).get(j);
+                            int pop = getHierarchy().get(i).get(j).getPopulation();
+                            for(int k = 0; k<pop; k++)
+                            {
+                                randomEvent(s.traverse(k));
+                            }
+                        }
+                    }
+                }
+            }
+    }
 }
